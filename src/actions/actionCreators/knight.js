3018723283b902c1d01a4b59e9d608b83e73e639
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2018-03-20 16:52:40 +0900

    [clean] 不要なファイル削除

--- src/actions/actionCreators/knight.js
+++
@@ -1,8 +0,0 @@
-import {MOVE_KNIGHT} from "../actionTypes/knight";
-
-export const move_knight = (toX, toY) => {
-	return {
-		type: MOVE_KNIGHT,
-		position: [toX, toY]
-	}
-}
--- src/actions/actionTypes/knight.js
+++
@@ -1,1 +0,0 @@
-export const MOVE_KNIGHT = 'MOVE_KNIGHT'
